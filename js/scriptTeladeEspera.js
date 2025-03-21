e58ecb31ded17cb9fd35faaabab7023c9aa0810b
jonas Tillmann Junior <jonastillmann19@gmail.com>
2025-03-21 19:42:20 -0300

    add mod rev HomePage

--- js/scriptTeladeEspera.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    var width = window.screen.width;
-    var height = window.screen.height;
-    console.log('Resolução da tela: ' + width + 'x' + height + ' - scriptTeladeEspera.js OK');
-});
-
